9d51628b1c2951da05aa05f2764e9c573879dc6a
ZhuangCd <balogh.nori22@gmail.com>
2023-09-28 11:28:53 +0200

    Cannot write file

--- calculator.py
+++ calculator.py
@@ -1,40 +1,41 @@
 import os
 
-input_txt = "/home/lapos-erdo/Desktop/calculator/numbers.txt"
-output_txt = "/home/lapos-erdo/Desktop/calculator/results.txt"
+input_file = "/home/lapos-erdo/Desktop/calculator/numbers.txt"
+output_file = "/home/lapos-erdo/Desktop/calculator/results.txt"
 
 
-def read_number_pairs(input_file):
-    if not (os.path.exists(input_file) or not os.access(input_file, os.R_OK)):
+def validate_file(input_file):
+    if not (os.path.exists(input_file) and os.access(input_file, os.R_OK)):
         exit(1)
+
+
+def collect_numbers(input_file):
+    number_list = []
     with open(input_file, "r") as file:
-        lines = file.readlines()
-
-    number_pairs = []
-    for line in lines:
-        num1, num2 = map(float, line.strip().split())
-        number_pairs.append((num1, num2))
-
-    return number_pairs
+        for line in file:
+            line_numbers = list(map(float, line.strip().split()))
+            number_list.extend(line_numbers)
+    return number_list
 
 
-def calculate_and_print_results(number_pairs):
+def add_numbers(number_list):
     results = []
-    for num1, num2 in number_pairs:
-        result = num1 + num2
-        results.append(result)
-
+    for i in range(0, len(number_list), 2):
+        if i + 1 < len(number_list):
+            result = number_list[i] + number_list[i+1]
+            results.append(result)
     return results
 
 
 def write_results_to_file(output_file, results):
-    with open(output_file, "w") as file:
+    with open(output_file, 'w') as file:
         for result in results:
-            file.write(str(result) + "\n")
+            file.write(str(result) + '\n')
 
 
 if __name__ == "__main__":
-    if os.path.exists(input_txt):
-        number_pairs = read_number_pairs(input_txt)
-        results = calculate_and_print_results(number_pairs)
-        write_results_to_file(output_txt, results)
+    if os.path.exists(input_file):
+        validate_file(input_file)
+        number_list = collect_numbers(input_file)
+        results = add_numbers(number_list)
+        write_results_to_file(output_file, results) 
