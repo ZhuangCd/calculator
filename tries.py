411ae27bb13b640bdbd7f5f477aa77d2f963a1ba
ZhuangCd <balogh.nori22@gmail.com>
2023-09-28 16:18:21 +0200

    Cannot write to file

--- tries.py
+++ tries.py
@@ -1,38 +1,25 @@
 import os
 
-def read_number_pairs(input_file):
-    """Reads number pairs from a file and returns them as a list."""
-    with open(input_file, 'r') as file:
-        lines = file.readlines()
-    
-    number_pairs = []
-    for line in lines:
-        num1, num2 = map(int, line.strip().split())
-        number_pairs.append((num1, num2))
-    
-    return number_pairs
+input_file = "/home/lapos-erdo/Desktop/calculator/numbers.txt"
+output_file = "/home/lapos-erdo/Desktop/calculator/results.txt"
 
-def calculate_and_print_results(number_pairs):
-    """Calculates the sum of number pairs and prints them."""
-    results = []
-    for num1, num2 in number_pairs:
-        result = num1 + num2
-        results.append(result)
-        print(result)
+def validate_file(input_file):
+    if not (os.path.exists(input_file) and os.access(input_file, os.R_OK)):
+        print(f"Input file '{input_file}' does not exist or is not readable.")
+        exit(1)
 
 def write_results_to_file(output_file, results):
-    """Writes results to an output file."""
-    with open(output_file, 'w') as file:
-        for result in results:
-            file.write(str(result) + '\n')
+    try:
+        with open(output_file, "w") as file:
+            for number in results:
+                formatted_number = "{:.1f}".format(number)
+                file.write(f"{formatted_number}\n")
+    except Exception as e:
+        print(f"An error occurred while writing to '{output_file}': {e}")
 
 if __name__ == "__main__":
-    input_txt = '/home/lapos-erdo/Desktop/calculator/numbers.txt'
-    output_txt = '/home/lapos-erdo/Desktop/calculator/results.txt'
-
-    if os.path.exists(input_txt):
-        number_pairs = read_number_pairs(input_txt)
-        calculate_and_print_results(number_pairs)
-        write_results_to_file(output_txt, number_pairs)
-    else:
-        print(f"Input file '{input_txt}' not found.")
+    if os.path.exists(input_file):
+        validate_file(input_file)
+        number_list = collect_numbers(input_file)
+        results = add_numbers(number_list)
+        write_results_to_file(output_file, results)
